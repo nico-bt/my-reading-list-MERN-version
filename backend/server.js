c7b12db471f28d5c8442614cda0a4a2a970623b7
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-18 00:02:45 -0300

    Add Routes and enable cors and json

--- backend/server.js
+++ backend/server.js
@@ -5,42 +5,16 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 
+//Cors and json
+var cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
 //Database
 const mongoose = require("mongoose")
 
-
-app.get("/", (req, res)=>{
-    res.json({msg: "Home Page"})
-})
-
-
-//Try to get data from database with the Book model
-const Book = require("./models/BookModel")
-
-app.get("/books", async (req, res)=>{
-    try {
-        const books = await Book.find({})
-        res.status(200).json(books)
-    } catch (error) {
-        res.status(400).json(error.message)
-    }
-})
-
-//Try to create data object with the Book model and save in database
-app.get("/books/new", async (req, res)=>{
-    const newBook = {
-        title: "Rayuela", 
-        author: "Julio Cortazar", 
-        link: "https://www.amazon.com/-/es/gp/product/B01FV29V6U/"
-    }
-    try {
-        const book = await Book.create(newBook)
-        res.status(200).json(book)
-    } catch (error) {
-        res.status(400).json(error.message)
-    }
-})
-
+//Routes
+app.use("/api/books", require("./routes/books"))
 
 //Connect to DB and run app
 mongoose.connect(process.env.MONGODB_URI)
