f871e6345309a873d347aba63c9c97591454fa7b
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-18 17:04:18 -0300

    Fetch data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import React, {useEffect, useState} from 'react'
+import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [books, setBooks] = useState([])
+
+    // Fetch data
+    useEffect(() => {
+        const fetchBooks = async ()=>{
+            const response = await fetch("http://localhost:4000/api/books")
+            const data = await response.json()
+            if(response.ok){
+                setBooks(data)
+            }
+        }
+        fetchBooks()
+    }, [])
+
   return (
     <div>
       <Navbar />
+      <BookList books={books} />
     </div>
   );
 }
