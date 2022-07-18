0324551d1ae392cf0be62b8b4c05e82c4ea98fa3
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-17 22:09:59 -0300

    Add connection to database. Add mongoose and dotenv packages

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
+//Enviroment variables
+require('dotenv').config()
+
+// Express
 const express = require("express")
 const app = express()
+
+//Database
+const mongoose = require("mongoose")
+
 
 app.get("/", (req, res)=>{
     res.json({msg: "Home Page"})
 })
 
-app.listen(4000,(req, res)=>{
-    console.log("listening on port 4000")
+app.get("/books", (req, res)=>{
+    res.json({msg: "GET All Books"})
 })
+
+//Connect to DB and run app
+mongoose.connect(process.env.MONGODB_URI)
+    .then(
+        app.listen(process.env.PORT, ()=>{
+            console.log(`Connected to DB & running on port: ${process.env.PORT}`)
+        })
+    )
+    .catch(
+        (err)=>console.log(err)
+    )
