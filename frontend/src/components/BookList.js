aa5a481473eb1097ddb8a14c8e521899c0ebe64a
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-18 21:30:08 -0300

    Add Book component

--- frontend/src/components/BookList.js
+++ frontend/src/components/BookList.js
@@ -1,13 +1,11 @@
+import Book from "./Book"
 import "./BookList.css"
 
 function BookList({books}) {
   return (
     <div className='container'>
-        {books.map(book => (
-            <div key={book._id} className="book">
-                <h1>{book.title}</h1>
-                <h2>{book.author}</h2>
-            </div>
+      {books.map(book => (
+        <Book key={book._id} book={book}/>
         ))}
     </div>
   )
