6e2b25021898a71922d6234218d7ed088c61a514
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-20 00:43:56 -0300

    Add BookContext making available to all the components: {books, dispatch}

---
+++ frontend/src/context/BookContext.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react'
+import { createContext } from "react";
+
+// Create a global state, available for all components/pages 
+// and keep in sync the db with local data --> [books]
+
+export const BookContext = createContext()
+
+export const booksReducer = (state, action)=>{
+    switch (action.type) {
+        case "SET_ALL_BOOKS":
+            return {
+                books: action.payload
+            }
+        case "CREATE_BOOK":
+            return {
+                books: [action.payload, ...state.books]
+            }    
+        case "DELETE_BOOK":
+            return {
+                // get the id as payload and filter actual state
+                books: state.books.filter(item => item._id != action.payload)
+            }
+        default:
+            return state
+    }
+}
+
+export function BookContextProvider({children}) {
+    const [state, dispatch] = useReducer( booksReducer, { books: [] } ) //useReducer(reducer, state)
+
+  return (
+    <BookContext.Provider value={{...state, dispatch}}>
+        {children}
+    </BookContext.Provider>
+  )
+}
