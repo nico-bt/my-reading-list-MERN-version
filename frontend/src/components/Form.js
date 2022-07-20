7b5581c83a48b7c5c94a89594e9bbf409d069a59
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-19 22:20:55 -0300

    Add Form component. Not in sync with local state yet. Have to refresh for get db in sync with frontend for now

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import "./Form.css"
+
+function Form({setShowForm}) {
+
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [link, setLink] = useState("")
+    const [error, setError] = useState(null)
+    const [submittedEmpty, setSubmittedEmpty] = useState(false)
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        if(!title || !author){
+            setSubmittedEmpty(true)
+            return
+        }
+        try {
+            const response = await fetch("http://localhost:4000/api/books", {
+                method: "POST",
+                body: JSON.stringify({title, author, link}),
+                headers: {"Content-Type": "application/json"}
+            })
+            if(response.ok){
+                setTitle("")
+                setAuthor("")
+                setLink("")
+                setSubmittedEmpty(false)
+                setShowForm(false)
+            }
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+  return (
+    <div  className="form-container">
+        <form className='form-control' onSubmit={handleSubmit}>
+            <span className= "material-symbols-outlined close-btn" onClick={()=>setShowForm(false)}>disabled_by_default</span>
+            <input 
+                type="text"
+                placeholder='Title' 
+                value={title} 
+                onChange={(e)=>{setTitle(e.target.value)}}
+                className= {(submittedEmpty && !title)? "error":""}>
+            </input>
+            
+            <input 
+                type="text"
+                placeholder='Author' 
+                value={author} 
+                onChange={(e)=>{setAuthor(e.target.value)}}
+                className= {(submittedEmpty && !author)? "error":""}>
+            </input>
+            
+            <input 
+                type="text"
+                placeholder='Optional link - https://amazon.com/your-book' 
+                value={link} 
+                onChange={(e)=>{setLink(e.target.value)}}>
+            </input>
+
+            <button type='submit' className='submit-btn'>Add Book</button>
+
+            {error && <div className='error'>{error}</div>}
+        </form>
+    </div>
+  )
+}
+
+export default Form
